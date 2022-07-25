584cf98e5bd1412a10f330bc12587368d90d5fcf
theringsofsaturn <emiliankasemi@gmail.com>
2022-07-25 03:04:09 +0200

    read.me

--- DataStructures/HashTableDataStructures/CreateHashTable.js
+++ DataStructures/HashTableDataStructures/CreateHashTable.js
@@ -1,19 +1,136 @@
 class HashTable {
-    constructor(size){
-      this.data = new Array(size);
+  constructor(size) {
+    this.data = new Array(size);
+  }
+
+  _hash(key) {
+    let hash = 0;
+    for (let i = 0; i < key.length; i++) {
+      hash = (hash + key.charCodeAt(i) * i) % this.data.length;
     }
-  
-    _hash(key) {
-      let hash = 0;
-      for (let i =0; i < key.length; i++){
-          hash = (hash + key.charCodeAt(i) * i) % this.data.length
+    return hash;
+  }
+
+  set(key, value) {
+    let address = this._hash(key);
+    if (!this.data[address]) {
+      this.data[address] = [];
+    }
+    this.data[address].push([key, value]);
+    // return this.data;
+  }
+
+  get(key) {
+    let address = this._hash(key);
+    let currentBucket = this.data[address];
+    if (currentBucket) {
+      for (let i = 0; i < currentBucket.length; i++) {
+        if (currentBucket[i][0] === key) {
+          return currentBucket[i][1];
+        }
       }
-      return hash;
+    }
+    return undefined;
+  }
+
+  keys() {
+    let keys = [];
+    for (let i = 0; i < this.data.length; i++) {
+      if (this.data[i]) {
+        for (let j = 0; j < this.data[i].length; j++) {
+          keys.push(this.data[i][j][0]);
+        }
+      }
+    }
+    return keys;
+  }
+  values() {
+    let values = [];
+    for (let i = 0; i < this.data.length; i++) {
+      if (this.data[i]) {
+        for (let j = 0; j < this.data[i].length; j++) {
+          values.push(this.data[i][j][1]);
+        }
+      }
+    }
+    return values;
+  }
+
+  remove(key) {
+    let address = this._hash(key);
+    let currentBucket = this.data[address];
+    if (currentBucket) {
+      for (let i = 0; i < currentBucket.length; i++) {
+        if (currentBucket[i][0] === key) {
+          currentBucket.splice(i, 1);
+        }
+      }
     }
   }
-  
-  const myHashTable = new HashTable(50);
-  myHashTable.set('grapes', 10000)
-  myHashTable.get('grapes')
-  myHashTable.set('apples', 9)
-  myHashTable.get('apples')
+
+  clear() {
+    this.data = [];
+  }
+
+  size() {
+    let count = 0;
+    for (let i = 0; i < this.data.length; i++) {
+      if (this.data[i]) {
+        count += this.data[i].length;
+      }
+    }
+    return count;
+  }
+
+  isEmpty() {
+    return this.size() === 0;
+  }
+
+  contains(key) {
+    return this.get(key) !== undefined;
+  }
+
+  toString() {
+    let string = "";
+    for (let i = 0; i < this.data.length; i++) {
+      if (this.data[i]) {
+        string += `${i}: `;
+        for (let j = 0; j < this.data[i].length; j++) {
+          string += `[${this.data[i][j][0]}, ${this.data[i][j][1]}] `;
+        }
+        string += "\n";
+      }
+    }
+    return string;
+  }
+
+  _resize(newSize) {
+    let oldData = this.data;
+    this.data = new Array(newSize);
+    for (let i = 0; i < oldData.length; i++) {
+      if (oldData[i]) {
+        for (let j = 0; j < oldData[i].length; j++) {
+          this.set(oldData[i][j][0], oldData[i][j][1]);
+        }
+      }
+    }
+  }
+
+  _resizeIfNeededForRemove() {
+    if (this.size() / this.data.length < 0.25) {
+      this._resize(this.data.length / 2);
+    }
+  }
+
+  _resizeIfNeededForAdd() {
+    if (this.size() / this.data.length > 0.75) {
+      this._resize(this.data.length * 2);
+    }
+  }
+}
+
+const myHashTable = new HashTable(50);
+myHashTable.set("grapes", 10000);
+myHashTable.get("grapes");
+myHashTable.set("apples", 9);
+myHashTable.get("apples");
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# data-structures-and-algorithms
+# Data Structures & Algorithms
+
+##
+
+![](https://res.cloudinary.com/dqiyjy9ye/image/upload/v1658710815/random/ds_alg_osyqnu.png)
