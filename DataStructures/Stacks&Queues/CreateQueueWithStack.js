3db0140cb2fd00f285073972452415a868942792
theringsofsaturn <emiliankasemi@gmail.com>
2022-07-27 03:52:23 +0200

    Create A queue with two stacks

---
+++ DataStructures/Stacks&Queues/CreateQueueWithStack.js
@@ -0,0 +1,21 @@
+// Create a queue with stacks
+
+class Queue {
+  constructor() {
+    this.stack1 = new Stack();
+    this.stack2 = new Stack();
+  }
+
+  enqueue(value) {
+    this.stack1.push(value);
+  }
+
+  dequeue() {
+    if (this.stack2.isEmpty()) {
+      while (!this.stack1.isEmpty()) {
+        this.stack2.push(this.stack1.pop());
+      }
+    }
+    return this.stack2.pop();
+  }
+}
