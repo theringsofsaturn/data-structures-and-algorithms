7707cc0785c904521199d97ada33cd4de0cd204a
theringsofsaturn <emiliankasemi@gmail.com>
2022-07-27 02:42:02 +0200

    Create a Stack with Linked List with methods: push(), pop(), peek(), clear(), isEmpty() etc

---
+++ DataStructures/Stacks&Queues/CreateStack.js
@@ -0,0 +1,85 @@
+// Stack Implementantion in JavaScript
+
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.next = null;
+  }
+}
+
+class Stack {
+  constructor() {
+    this.top = null;
+    this.bottom = null;
+    this.length = 0;
+  }
+
+  peek() {
+    // simply return top
+    return this.top;
+  }
+
+  push(data) {
+    // create a new node
+    const newNode = new Node(data);
+    // if stack is empty
+    if (this.length === 0) {
+      // set both top and bottom to new node
+      this.top = newNode;
+      this.bottom = newNode;
+    } else {
+      // if stack is not empty
+      newNode.next = this.top; // set newNode's next to top
+      this.top = newNode; // set top to newNode
+    }
+    this.length++;
+    return this;
+  }
+
+  pop() {
+    // if stack is empty
+    if (this.length === 0) {
+      return null;
+    }
+    // if stack has one element
+    let poppedNode = this.top; // set poppedNode to top
+    this.top = this.top.next; // set top to top's next
+    this.length--; // decrement length
+    return poppedNode;
+  }
+
+  isEmpty() {
+    return this.length === 0;
+  }
+
+  length() {
+    return this.length;
+  }
+
+  print() {
+    let current = this.top;
+    while (current) {
+      console.log(current.data);
+      current = current.next;
+    }
+  }
+
+  clear() {
+    this.top = null;
+    this.bottom = null;
+    this.length = 0;
+  }
+}
+
+let myStack = new Stack();
+console.log(myStack.push("Google"));
+console.log(myStack.push("Facebook"));
+console.log(myStack.push("Twitter"));
+console.log(myStack.push("Linkedin"));
+console.log(myStack.push("Instagram"));
+console.log(myStack.push("Snapchat"));
+console.log(myStack.pop());
+
+console.log(myStack.print());
+console.log(myStack.isEmpty());
+// console.log(myStack.length());
