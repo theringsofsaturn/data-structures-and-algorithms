7f0a847e85d82c835f19d2e96c040fa54c87a818
theringsofsaturn <emiliankasemi@gmail.com>
2022-08-08 19:14:30 +0200

    Find the index value of the Fibonacci sequence with iteration and recursion

--- Algorithms/Recursion/Fibonacci.js
+++ Algorithms/Recursion/Fibonacci.js
@@ -2,7 +2,8 @@
 // 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, …
 // The pattern of the sequence is that each value is the sum of the 2 previous values, that means that for N=5 → 2+3
 
-let fibonacciArray = [];
+// Iterative solution
+let fibonacciArray = []; // or we can initialize the array with [0, 1], then the condition if(n < 2) => return n.
 let fibonacci = (n) => {
   if (n === 0) {
     return 0;
@@ -13,23 +14,25 @@ let fibonacci = (n) => {
   }
 };
 
-for (let i = 0; i <= 15; i++) {
+for (let i = 0; i <= 15 + 1; i++) {
   fibonacciArray.push(fibonacci(i));
 }
-console.log(fibonacciArray);
-console.log(fibonacci(7));
 
-function findIndexValueOfFibonacciSequence(n) {
+console.log("Fibonacci sequence", fibonacciArray);
+console.log("Iterative solution: ", fibonacci(7));
+
+// Recursive solution
+function findIndexValueOfFibonacciRecursive(n) {
   if (n === 0) {
     return 0;
   } else if (n === 1) {
     return 1;
   } else {
     return (
-      findIndexValueOfFibonacciSequence(n - 1) +
-      findIndexValueOfFibonacciSequence(n - 2)
+      findIndexValueOfFibonacciRecursive(n - 1) +
+      findIndexValueOfFibonacciRecursive(n - 2)
     );
   }
 }
 
-console.log(findIndexValueOfFibonacciSequence(6));
+console.log("Recursive solution: ", findIndexValueOfFibonacciRecursive(6));
