a00e3b822f5a36950a29029e8d73ba1c71ca6119
theringsofsaturn <emiliankasemi@gmail.com>
2022-08-09 16:05:37 +0200

    Strive School exercise - Module 1 - Day 4 - Strings

---
+++ StriveSchool/M1-D4/strings.js
@@ -0,0 +1,28 @@
+let firstString = 'strive school'
+let secondString = " will make it"
+
+// template literal
+let thirdString = `this is a backtick
+                    limited string`
+
+let nameOfStriver = 'Ingrid'
+
+console.log(nameOfStriver + ' ' + 'is a student of Strive School')
+
+console.log(`${nameOfStriver} is a student of Strive School`)
+
+let newString = nameOfStriver + secondString
+
+console.log('stefano'.length)
+
+// in javascript you can invoke methods or properties
+// methods will require ()
+// properties will not
+
+console.log('stefano'.slice(0,3))
+
+let longString = "School Strive will make you a developer. You just need to Strive!"
+
+console.log(longString.replace('Strive', 'Our'))
+
+console.log(longString.indexOf('will make'))
