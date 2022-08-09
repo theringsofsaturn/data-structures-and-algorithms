9e5c8f4e32ff200df514ebc15296977c6abae6a3
theringsofsaturn <emiliankasemi@gmail.com>
2022-08-09 17:40:23 +0200

    Insertion sorting algorithm

---
+++ Algorithms/Sorting/InsertionSort.js
@@ -0,0 +1,29 @@
+// It might not be the most effiecient either, but when it comes to small data sets, or data sets that are nearly sorted, you would want to use insertion algorithm over the other sorting algorithms, because it is the fastest.
+
+// Insertion sort example algorithm
+
+const numbers = [99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0];
+
+function insertionSort(array) {
+  const length = array.length;
+  for (let i = 0; i < length; i++) {
+    if (array[i] < array[0]) {
+      //move number to the first position
+      array.unshift(array.splice(i, 1)[0]);
+    } else {
+      // only sort number smaller than number on the left of it. This is the part of insertion sort that makes it fast if the array is almost sorted.
+      if (array[i] < array[i - 1]) {
+        //find where number should go
+        for (var j = 1; j < i; j++) {
+          if (array[i] >= array[j - 1] && array[i] < array[j]) {
+            //move number to the right spot
+            array.splice(j, 0, array.splice(i, 1)[0]);
+          }
+        }
+      }
+    }
+  }
+}
+
+insertionSort(numbers);
+console.log(numbers);
