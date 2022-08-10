eb58f74e5814f6d06dc01ecca40913cf60c84684
theringsofsaturn <emiliankasemi@gmail.com>
2022-08-10 15:57:36 +0200

    Depth first search algorithm

--- Algorithms/BFS&DFS/BFS&DFS.js
+++ Algorithms/BFS&DFS/BFS&DFS.js
@@ -16,3 +16,8 @@ tree.insert(1);
 //1  6  15  170
 
 BFS[(9, 4, 20, 1, 6, 15, 170)]; // this is the order in which the nodes are visited
+
+// Depth first search is a search algorithm that starts at the root node and explores as far as possible down each branch before backtracking. Usually going down at left side, until it reaches the end child, then go up again to find if there is a child on the right side at that node.
+
+// With the above tree example, the order of DFS would be:
+DFS[(9, 4, 1, 6, 20, 15, 170)];
