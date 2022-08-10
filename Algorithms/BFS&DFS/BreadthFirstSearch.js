ae7ddadbb0569c477a742e5597a386c0028c8b44
theringsofsaturn <emiliankasemi@gmail.com>
2022-08-10 15:44:00 +0200

    Breadth first search algorithm

---
+++ Algorithms/BFS&DFS/BreadthFirstSearch.js
@@ -0,0 +1,9 @@
+// Breadth first search is a search algorithm that starts at the root node and explores as far as possible along each branch before backtracking. It will usually go from left to right then next level, then left to right again, and so on.
+
+// Like in this tree example:
+
+//      9
+//  4      20
+//1  6  15  170
+
+BFS[(9, 4, 20, 1, 6, 15, 170)]; // this is the order in which the nodes are visited
