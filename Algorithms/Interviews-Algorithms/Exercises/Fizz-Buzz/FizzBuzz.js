c3cd4d74b5a1138c6f3b499a2510b010ec17fd83
theringsofsaturn <emiliankasemi@gmail.com>
2022-08-14 04:11:58 +0200

    FizzBuzz algorithm

--- Algorithms/Interviews-Algorithms/Exercises/Fizz-Buzz/FizzBuzz.js
+++ Algorithms/Interviews-Algorithms/Exercises/Fizz-Buzz/FizzBuzz.js
@@ -1,22 +1,33 @@
-// Write a program that prints the numbers from 1 to 100. But for multiples of three print “Fizz” instead of the number and for the multiples of five print “Buzz”. For numbers which are multiples of both three and five print “FizzBuzz”.
+// --- Directions
+// Write a program that console logs the numbers
+// from 1 to n. But for multiples of three print
+// “fizz” instead of the number and for the multiples
+// of five print “buzz”. For numbers which are multiples
+// of both three and five print “fizzbuzz”.
+// --- Example
+//   fizzBuzz(5);
+//   1
+//   2
+//   fizz
+//   4
+//   buzz
 
 // Solution:
 
 var fizzBuzz = function (n) {
-    let result = [];
-    for (let i = 1; i <= n; i++) {
-      if (i % 3 === 0 && i % 5 === 0) {
-        result.push("FizzBuzz");
-      } else if (i % 3 === 0) {
-        result.push("Fizz");
-      } else if (i % 5 === 0) {
-        result.push("Buzz");
-      } else {
-        result.push(i);
-      }
+  let result = [];
+  for (let i = 1; i <= n; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      result.push("FizzBuzz");
+    } else if (i % 3 === 0) {
+      result.push("Fizz");
+    } else if (i % 5 === 0) {
+      result.push("Buzz");
+    } else {
+      result.push(i);
     }
-    return result;
-  };
-  
-  console.log(fizzBuzz(100));
-  
+  }
+  return result;
+};
+
+console.log(fizzBuzz(100));
